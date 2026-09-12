a297a765b97dcea041b7d26950f6af140ca48cda
Aditya384197 <sky747874@gmail.com>
2026-09-12 11:03:34 +0530

    Add Color.kt with theme color definitions

---
+++ app/src/main/java/com/aditya/music/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.aditya.music.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AdityaIndigo = Color(0xFF6366F1)
+val AdityaIndigoDark = Color(0xFF4F46E5)
+val AdityaPink = Color(0xFFEC4899)
+val AdityaAmber = Color(0xFFF59E0B)
+
+val BackgroundDark = Color(0xFF0B0F19)
+val SurfaceDark = Color(0xFF161E2E)
+val CardDark = Color(0xFF1F293D)
+val TextPrimaryDark = Color(0xFFF8FAFC)
+val TextSecondaryDark = Color(0xFF94A3B8)
+
+val BackgroundLight = Color(0xFFF8FAFC)
+val SurfaceLight = Color(0xFFFFFFFF)
+val CardLight = Color(0xFFF1F5F9)
+val TextPrimaryLight = Color(0xFF0F172A)
+val TextSecondaryLight = Color(0xFF64748B)
+
+val AmoledBlack = Color(0xFF000000)
+val AmoledSurface = Color(0xFF0A0A0A)
