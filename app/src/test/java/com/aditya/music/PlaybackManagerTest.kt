f39b714dc088dc5f7c41934dad2ab34163aec295
Aditya384197 <sky747874@gmail.com>
2026-09-12 10:03:44 +0530

    Add files via upload

---
+++ app/src/test/java/com/aditya/music/PlaybackManagerTest.kt
@@ -0,0 +1,16 @@
+package com.aditya.music
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PlaybackManagerTest {
+
+    @Test
+    fun queueShuffling_producesValidPermutation() {
+        val list = listOf(1L, 2L, 3L, 4L, 5L)
+        val shuffled = list.shuffled()
+
+        assertEquals(list.size, shuffled.size)
+        assertTrue(shuffled.containsAll(list))
+    }
+}
