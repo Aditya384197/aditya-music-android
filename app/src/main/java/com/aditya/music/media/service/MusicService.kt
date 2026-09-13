2723ae578b8780e80f8d37b6d7e73fd307b0f3a6
Aditya384197 <sky747874@gmail.com>
2026-09-13 20:59:05 +0530

    Refactor import for MediaLibrarySession

--- app/src/main/java/com/aditya/music/media/service/MusicService.kt
+++ app/src/main/java/com/aditya/music/media/service/MusicService.kt
@@ -19,7 +19,7 @@ import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaLibraryService
-import androidx.media3.session.MediaLibrarySession
+import androidx.media3.session.MediaLibraryService.MediaLibrarySession
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaStyleNotificationHelper
 import com.aditya.music.MainActivity
