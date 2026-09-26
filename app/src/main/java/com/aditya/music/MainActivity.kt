7dbbaa174f24b3f971d2f329b6b3988d17d18de0
Aditya384197 <sky747874@gmail.com>
2026-09-26 07:20:34 +0530

    Import width for layout adjustments in MainActivity

--- app/src/main/java/com/aditya/music/MainActivity.kt
+++ app/src/main/java/com/aditya/music/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.VolumeDown
 import androidx.compose.material.icons.rounded.VolumeUp
